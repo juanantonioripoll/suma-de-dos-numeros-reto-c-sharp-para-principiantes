c1ad3f5d5700ef1ea920e1a70ef067621b580477
anonymous <anonymous@ANONYMOUSDEV>
2024-01-03 11:55:15 +0100

    Agregar archivos de proyecto.

---
+++ Suma de dos números/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_dos_números
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(12 + 15);
+            Console.Read();
+        }
+    }
+}
